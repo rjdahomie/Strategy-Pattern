e9567ce1a2bdc0fe4fca480b41e553ccc1f63e0c
rjdahomie <rasajahromi@gmail.com>
2022-05-16 22:44:05 -0700

    Finished select_or.hpp
    *Implemented the select_or hpp file

--- select_or.hpp
+++ select_or.hpp
@@ -7,30 +7,28 @@ using namespace std;
 
 class Select_Or: public Select
 {
-    public:
-        Select* s1;
-	Select* s2;
+    protected:
+        Select* s1 = nullptr;
+	Select* s2 = nullptr;
 
     private:
         Select_Or(Select *s1, Select* s2)
         {
-
+	    s1 = s1;
+	    s2 = s2;
         }
 
         ~Select_Or()
         {
-
+	    delete s1;
+	    delete s2;
         }
 
         bool select(const Spreadsheet* sheet, int row) const
         {
-
+	    return (s1->select(sheet, row) || s2->select(sheet,row));
         }
 };
-
-
-
-
 
 #endif //__SELECTNOT_HPP__
 
