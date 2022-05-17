16c5c7fe295b53619ae86a58bff8bd9acb204993
rjdahomie <rasajahromi@gmail.com>
2022-05-16 22:48:56 -0700

    Finished the select_and.hpp
    *Implemented the select_and.hpp file

--- select_and.hpp
+++ select_and.hpp
@@ -7,24 +7,26 @@ using namespace std;
 
 class Select_And: public Select
 {
-    public:
+    protected:
         Select* s1;
 	Select* s2;
 
     private:
         Select_And(Select* s1, Select* s2)
         {
-
+	    s1 = s1;
+	    s2 = s2;
         }
 
         ~Select_And()
         {
-
+	    delete s1;
+	    delete s2;
         }
 
         bool select(const Spreadsheet* sheet, int row) const
         {
-
+	    return (s1->select(sheet, row) && s2->select(sheet, row));
         }
 };
 
