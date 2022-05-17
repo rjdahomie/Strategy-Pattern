75e32c02774881768f71583cc600cc38c5c13981
rjdahomie <rasajahromi@gmail.com>
2022-05-16 22:51:27 -0700

    Finished the select_not.hpp
    *Implemented the select_not.hpp file

--- select_not.hpp
+++ select_not.hpp
@@ -7,23 +7,23 @@ using namespace std;
 
 class Select_Not: public Select
 {
-    public:
+    protected:
 	Select* s;
 
     private:
 	Select_Not(Select *s)
 	{
-
+	    s = s;
 	}
 
 	~Select_Not()
 	{
-
+	    delete s;
 	}
 
 	bool select(const Spreadsheet* sheet, int row) const
 	{
-
+	    return (!(s->select(sheet, row)));
 	}
 };
 
